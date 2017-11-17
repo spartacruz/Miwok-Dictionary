a0b6127feb7df0c13d57b4c9c916032e68e7ce9e
Yuri <yuri.barru@gmail.com>
2017-11-17 22:23:08 +0700

    Add tabs to ViewPager.

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -1,5 +1,6 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -8,12 +9,29 @@ import android.support.v4.app.FragmentPagerAdapter;
  * Created by phi3d on 11/15/2017.
  */
 
+/**
+ * {@link CategoryAdapter} is a {@link FragmentPagerAdapter} that can provide the layout for
+ * each list item based on a data source which is a list of {@link Word} objects.
+ */
 public class CategoryAdapter extends FragmentPagerAdapter{
 
-    public CategoryAdapter(FragmentManager fm) {
+    private Context pageContext;
+
+    /**
+     * Create a new {@link CategoryAdapter} object.
+     *
+     * @param context is the context of the app
+     * @param fm is the fragment manager that will keep each fragment's state in the adapter
+     *           across swipes.
+     */
+    public CategoryAdapter(Context context, FragmentManager fm) {
         super(fm);
+        pageContext = context;
     }
 
+    /**
+     * Return the {@link Fragment} that should be displayed for the given page number.
+     */
     @Override
     public Fragment getItem(int position) {
         if (position == 0) {
@@ -27,6 +45,23 @@ public class CategoryAdapter extends FragmentPagerAdapter{
         }
     }
 
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        if (position == 0) {
+            return pageContext.getString(R.string.category_numbers);
+        } else if (position == 1){
+            return pageContext.getString(R.string.category_family);
+        } else if (position == 2){
+            return pageContext.getString(R.string.category_colors);
+        } else {
+            return pageContext.getString(R.string.category_phrases);
+        }
+    }
+
+    /**
+     * Return the total number of pages.
+     */
     @Override
     public int getCount() {
         return 4;
--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -17,6 +17,7 @@ package com.example.android.miwok;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -35,9 +36,14 @@ public class MainActivity extends AppCompatActivity {
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
         // Create an adapter that knows which fragment should be shown on each page
-        CategoryAdapter adapter = new CategoryAdapter (getSupportFragmentManager());
+        CategoryAdapter adapter = new CategoryAdapter (this, getSupportFragmentManager());
 
         // Set the adapter onto the view pager
         viewPager.setAdapter(adapter);
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
